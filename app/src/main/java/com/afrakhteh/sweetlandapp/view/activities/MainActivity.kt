5618a20540b59a91d026cc4cce23295494c83c97
melikaafrakhteh <45101381+melikaafrakhteh@users.noreply.github.com>
2021-04-28 16:29:23 +0430

    setup navigation

--- app/src/main/java/com/afrakhteh/sweetlandapp/view/activities/MainActivity.kt
+++ app/src/main/java/com/afrakhteh/sweetlandapp/view/activities/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.afrakhteh.sweetlandapp.view.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.afrakhteh.sweetlandapp.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+        window.decorView.apply {
+            systemUiVisibility =
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+
+        setNavigation()
+    }
+
+    private fun setNavigation() {
+        main_activity_bottonnavigation_menu.setupWithNavController(fragment.findNavController())
     }
 }
