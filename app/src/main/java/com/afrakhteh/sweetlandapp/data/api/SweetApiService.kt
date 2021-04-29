86bc0747bc582fc1c1e4065263c2dfb1ca8e76b3
melikaafrakhteh <45101381+melikaafrakhteh@users.noreply.github.com>
2021-04-29 15:06:45 +0430

    setup  retrofit service class

---
+++ app/src/main/java/com/afrakhteh/sweetlandapp/data/api/SweetApiService.kt
@@ -0,0 +1,21 @@
+package com.afrakhteh.sweetlandapp.data.api
+
+import com.afrakhteh.sweetlandapp.data.model.SweetsModel
+import com.afrakhteh.sweetlandapp.util.UrlLinks
+import io.reactivex.Single
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class SweetApiService {
+    val api = Retrofit.Builder()
+            .baseUrl(UrlLinks.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(SweetsApi::class.java)
+
+    fun getSweets(): Single<List<SweetsModel>> {
+        return api.getSweets()
+    }
+}
